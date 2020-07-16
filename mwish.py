4a90d1aa888f489f4c78e27cf513e8b2bce1f57b
mwish <anmmscs_maple@qq.com>
2020-07-16 10:53:28 +0800

    [ADD] add junk script about me

---
+++ mwish.py
@@ -0,0 +1,6 @@
+print("Hi, I'm mwish! Glad to meet you!")
+
+name = {'mwish', 'mapleFU'}
+interests = ['anime', 'literature', 'sociology', 'science-fiction']
+learning = ['database systems', 'operating systems', 'distributed systems', 'linear algebra', 'C++', 'Golang']
+motto = ("So high, so low, so many things to know.", "Work healthily 50 years for ACGN.")
